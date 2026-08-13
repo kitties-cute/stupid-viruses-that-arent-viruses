bdedb4967aeab23be24a4a6a14382bd21a59b08a
kitties-cute <palomaetprincesse@gmail.com>
2026-08-13 23:14:27 +0200

    Update helloworld.ps1

--- helloworld.ps1
+++ helloworld.ps1
@@ -1,4 +1,4 @@
-# Encodes "Start-Process notepad.exe" into Base64, then runs it like a dropper
+# makes antiviruses think this is a payload but it actually is just hello wolrd (you can check it yourself if you feel like this is malicious but it isnt)
 $encoded = "UwB0AGEAcgB0AC0AUAByAG8AYwBlAHMAcwAgAG4AbwB0AGUAcABhAGQALgBlAHgAZQ=="
 $decoded = [System.Text.Encoding]::Unicode.GetString([System.Convert]::FromBase64String($encoded))
 Invoke-Expression $decoded
